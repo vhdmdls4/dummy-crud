ab16d033f8163836966ea7d92aa7357f137a91d7
Victor <vhduartelima@hotmail.com>
2024-12-03 23:28:04 -0300

    removed hello api

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ from fastapi import FastAPI
 from core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
-
-@app.get("/")
-def hello_api():
-    return {"msg":"Hello FastAPI🚀"}
