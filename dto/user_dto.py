d2705165c785bfefa477abc236cc8515e3432895
Victor <vhduartelima@hotmail.com>
2024-12-15 11:27:06 -0300

    feat: dto user class created

---
+++ dto/user_dto.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import datetime
+
+# provavelmente chamado de schema no contexto 
+class UserDTO(BaseModel):
+  id: Optional[int] = Field(default=1, description="Id do usuário")
+  name: str = Field(min_length=2, max_length=100, description="Nome do usuário.")
+  email: EmailStr = Field(description="E-mail do usuário")
+  is_active: bool = Field(default=True, description="Indica se o usuário está ativo")
+  
+  class Config:
+    orm_mode = True
